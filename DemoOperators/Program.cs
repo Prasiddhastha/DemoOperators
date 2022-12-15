908d7a8c267dd7b4737ed378813e639254783329
Prasiddha Shrestha <prasiddhastha7@gmail.com>
2022-12-16 00:30:40 +0545

    Add project files.

---
+++ DemoOperators/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace DemoOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var temp = a;
+            var d = temp++;
+            var b = 20;
+            var c = 30;
+            //Arithmetic
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a/b);
+            Console.WriteLine(a % b);
+            Console.WriteLine($"{temp}and{d}\n");
+            //comparison
+            Console.WriteLine(a>b);//false
+            Console.WriteLine(a < b);//true
+            Console.WriteLine(a == b);//false
+            Console.WriteLine(a!=b);//true
+            Console.WriteLine(a>=b);//false
+            Console.WriteLine(a <= b);//true
+            Console.WriteLine("\n");
+            //assignment
+            Console.WriteLine(a = 10);
+            Console.WriteLine(a +=1 );
+            Console.WriteLine(a -=1);
+            Console.WriteLine(a*=2);
+            Console.WriteLine(a /= 2);
+            Console.WriteLine("\n");
+            //logical(&&,||,!)
+            Console.WriteLine(c>a &&c>b);
+            //bitwise(&,|)
+
+
+
+
+
+            
+
+
+        }
+    }
+}
